58fc123d78a1ddb2c921a9d7d455710b9b304dcb
Muhammad Fazal <38839909+fazaltech@users.noreply.github.com>
2023-10-13 11:39:50 +0500

    The use of Commands in .NET MAUI

---
+++ MVVM/ViewModels/CommandsViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MVVMDemo.MVVM.ViewModels
+{
+    public class CommandsViewModel
+    {
+        public ICommand ClickCommand { get; }
+
+
+        public CommandsViewModel()
+        {
+            ClickCommand = new Command(() => 
+            {
+				App.Current.MainPage.DisplayAlert("Title", "message", "Ok");
+			});
+        }
+        
+	}
+}
